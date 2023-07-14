c444747c53ac88f6871dedb82e0ab753db6d090e
nivensd3 <nivensd3@gmail.com>
2023-07-14 16:46:11 +0100

    improved inputs

--- battleship.py
+++ battleship.py
@@ -3,8 +3,14 @@ from random import*
 
 #Board size is inputed and printed
 my_grid=[]
-board_size = eval(input("What size board do you want? enter one interger please?:  "))
-
+while True:
+    try:
+        board_size = eval(input("What size board do you want? enter one interger please?: "))
+        break
+    except NameError:
+        print("That's not a number")
+        
+    
 print("Sink Or Swim - Battleship")
 
 for x in range(board_size):
@@ -29,23 +35,33 @@ ship_column = random_c(my_grid)
 
 
 def Guesses():
-        for guess in range (4):
-            guess_column = int(input("what column do you want to hit: "))
-            guess_row = int(input('what row do you want to hit: '))
+        for guess in range (5):
+            
+            while True:
+                try:
+                    guess_column = int(input("what column do you want to hit: "))
+                    guess_row = int(input('what row do you want to hit: '))
+                    break
+                except ValueError:
+                    print("Not a coordinate on map")
                 
+                    
             if (guess_row -1) == ship_row and (guess_column -1) == ship_column:
-                print('you hit it!')            
+                print('you hit it!')
+                break            
             elif guess_column > board_size or guess_row > board_size:
                 print ("out of bounds")
             else:
                 print ("miss")
-        if guess == 5 :
-            print('Game over') 
+            if guess == 5 :
+                print('Game over') 
         guess = guess + 1 
         
     
         
 Guesses ()
+
+print("the ship was located at: " +  ship_column,ship_row)
                 
     
         
