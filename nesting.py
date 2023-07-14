6bdd9725c118912c0995d2730aed9905c422a893
langhston <langhstonadams09@gmail.com>
2023-07-14 16:07:56 +0100

    Second wave of code

--- nesting.py
+++ nesting.py
@@ -3,7 +3,15 @@ from random import*
 
 #Board size is inputed and printed
 my_grid=[]
-board_size = eval(input("What size board do you want? enter one interger please?:  "))
+while True:
+    try:
+        board_size = eval(input("What size board do you want? enter one interger please?:  "))
+        
+    except: 
+        NameError
+        print("That's not a number")
+    if type(board_size) == int:
+        break
 
 print("Sink Or Swim - Battleship")
 
@@ -25,24 +33,34 @@ def random_c(board_size):
     return randint(0, len(board_size[0])-1)
 
  
+
+
+ 
+ 
+ 
 def Guesses():
         for guess in range (5):
             guess_column = eval(input("what column do you want to hit: "))
             guess_row = eval(input('what row do you want to hit: '))
+            if type(guess_column or guess_row) == str:
+                print ("Please enter a number")
+                guess = guess + 0 
+                break
+                
+            btl_ship = guess_column and guess_row
             if guess_column > board_size or guess_row > board_size:
                 print ("out of bounds")
-                break
+                
             elif guess_column and guess_row == btl_ship:
-                print('you hit it!')
-        if guess == 5 :
+                
+               print('you hit it!')
+        if guess == 4 :
             print('Game over') 
         guess = guess + 1 
         
     
         
 Guesses ()
-                
-    
         
         
      
